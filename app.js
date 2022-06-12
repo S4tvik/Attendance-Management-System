95adac1a7245f4216e1b3d8e121066fcbc86807e
shahbaz <shahbazjahan9@gmail.com>
2022-06-12 20:10:34 +0530

    g

--- app.js
+++ app.js
@@ -62,7 +62,7 @@ MongoClient.connect(url, function (err, db) {
 });
 
 app.get('/', function (req, res) {
-    res.sendFile(__dirname + '/login.html');
+    res.sendfile('login.html');
 });
 
 app.post('/check', function (req, res) {
@@ -78,7 +78,7 @@ app.post('/check', function (req, res) {
             var query = { username: uname };
             dbo.collection("login").find(query).toArray(function (err, result) {
                 if (result.length == 0) {
-                    res.sendFile(__dirname + '/login1.html')
+                    res.sendFile(__dirname +'/login1.html')
                 }
                 else {
                     if (err) throw err;
@@ -88,13 +88,11 @@ app.post('/check', function (req, res) {
                         if (result[0].password == pword && result[0].type == "student") {
                             req.session.loggedin = true;
                             req.session.username = uname;
-                            req.session.type = "student";
                             res.redirect("/student-home");
                         }
                         if (result[0].password == pword && result[0].type == "teacher") {
                             req.session.loggedin = true;
                             req.session.username = uname;
-                            req.session.type = "teacher";
                             res.redirect("/teacher-home");
                         }
                     }
@@ -104,120 +102,73 @@ app.post('/check', function (req, res) {
         });
     }
     else {
-        res.sendFile(__dirname + '/login1.html');
+        res.send("enter details idiot");
     }
 });
 
 app.get('/student-home', function (req, res) {
-    if (req.session.loggedin && req.session.type == "student") {
+    if (req.session.loggedin) {
         uname = req.session.username;
-        var percentage = 0;
-        var length1 = 0
-        var percentage1 = 0;
-        var length2 = 0;
-        var percentage2 = 0;
         MongoClient.connect(url, function (err, db) {
             if (err) throw err;
             dbo = db.db("firstdb");
-            var query = { sid: uname, course: "iwt" };
-            dbo.collection("attendance").find(query).toArray(function (err, result1) {
+            var query = { sid: uname };
+            dbo.collection("attendance").find(query).toArray(function (err, result) {
                 if (err) throw err;
                 var total_classes_attended = 0;
-                length1 = result1.length;
-                for (let i = 0; i < length1; i++) {
-                    if (result1[i].present == "1") {
+                for (let i = 0; i < result.length; i++) {
+                    if (result[i].present == "1") {
                         total_classes_attended += 1;
                     }
                 }
-                percentage1 = total_classes_attended * 100 / length1;
-
+                // console.log(result.length);
+                // console.log(total_classes_attended);
+                var percent = total_classes_attended * 100 / result.length;
+                var percentage=parseFloat(percent).toFixed(2)+"%";
+                res.render(__dirname + "/student_home.ejs", { percentage: percentage, uname: uname });
+                db.close;
             });
-
-            var query = { sid: uname, course: "daa" };
-            dbo.collection("attendance").find(query).toArray(function (err, result2) {
-                if (err) throw err;
-                var total_classes_attended = 0;
-                length2 = result2.length;
-                for (let i = 0; i < length2; i++) {
-                    if (result2[i].present == "1") {
-                        total_classes_attended += 1;
-                    }
-                }
-                var percentage2 = total_classes_attended * 100 / length2;
-                percentage = ((percentage1 * length1 + percentage2 * length2) / (length1 + length2)).toFixed(2);
-                res.render(__dirname + "/student_home.ejs", { percentage: percentage, uname: uname, percentage1: percentage1, percentage2: percentage2 });
-            });
-            db.close;
         });
     } else {
-        res.sendFile(__dirname + "/notloggedin.html");
+        res.sendfile("notloggedin.html");
     }
 });
-app.get('/logout', function (req, res) {
-    req.session.loggedin = false;
-    res.sendFile(__dirname + "/login.html");
-});
 app.get('/teacher-home', function (req, res) {
-    if (req.session.loggedin && req.session.type == "teacher") {
+    if (req.session.loggedin) {
         uname = req.session.username;
         res.render(__dirname + "/teacher_home.ejs", { uname: uname });
     } else {
-        res.sendFile(__dirname + "/notloggedin.html");
+        res.sendfile("notloggedin.html");
     }
 });
 app.get('/teacher-class', function (req, res) {
-    if (req.session.loggedin && req.session.type == "teacher") {
+    if (req.session.loggedin) {
         res.render(__dirname + "/teacher_class.ejs", { uname: uname });
     } else {
-        res.sendFile(__dirname + "/notloggedin.html");
+        res.sendfile("notloggedin.html");
+    }
+});
+app.get('/teacher-class2', function (req, res) {
+    if (req.session.loggedin) {
+        res.render(__dirname + "/teacher_class2.ejs", { uname: uname });
+    } else {
+        res.sendfile("notloggedin.html");
     }
 });
 app.get('/teacher-form', function (req, res) {
-    if (req.session.loggedin && req.session.type == "teacher") {
+    if (req.session.loggedin) {
         date = req.session.date;
         res.render(__dirname + "/teacher_form.ejs", { date: date });
     } else {
-        res.sendFile(__dirname + "/notloggedin.html");
-    }
-});
-app.get('/teacher-form1', function (req, res) {
-    if (req.session.loggedin && req.session.type == "teacher") {
-        req.session.date = "10-06-2022";
-        res.redirect("/teacher-form");
-    } else {
-        res.sendFile(__dirname + "/notloggedin.html");
+        res.sendfile("notloggedin.html");
     }
 });
 app.get('/teacher-form2', function (req, res) {
-    if (req.session.loggedin && req.session.type == "teacher") {
-        req.session.date = "11-06-2022";
-        res.redirect("/teacher-form");
+    if (req.session.loggedin) {
+        date = req.session.date;
+        res.render(__dirname + "/teacher_form2.ejs", { date: date });
     } else {
-        res.sendFile(__dirname + "/notloggedin.html");
-    }
-});
-app.get('/teacher-form3', function (req, res) {
-    if (req.session.loggedin && req.session.type == "teacher") {
-        req.session.date = "12-06-2022";
-        res.redirect("/teacher-form");
-    } else {
-        res.sendFile(__dirname + "/notloggedin.html");
-    }
-});
-app.get('/teacher-form4', function (req, res) {
-    if (req.session.loggedin && req.session.type == "teacher") {
-        req.session.date = "13-06-2022";
-        res.redirect("/teacher-form");
-    } else {
-        res.sendFile(__dirname + "/notloggedin.html");
-    }
-});
-app.get('/teacher-form5', function (req, res) {
-    if (req.session.loggedin && req.session.type == "teacher") {
-        req.session.date = "14-06-2022";
-        res.redirect("/teacher-form");
-    } else {
-        res.sendFile(__dirname + "/notloggedin.html");
+        res.sendfile("notloggedin.html");
     }
 });
 app.post('/attendance-submit', function (req, res) {
@@ -263,8 +214,15 @@ app.post('/attendance-submit', function (req, res) {
         });
         res.redirect("/teacher-class");
     } else {
-        res.sendFile(__dirname + "/notloggedin.html");
+        res.sendfile("notloggedin.html");
     }
 });
 
-app.listen(3000);
+app.use((req, res, next) => {
+    res.status(404).sendfile("404notfound.html")
+  })
+
+
+app.listen(3000, function () {
+    console.log('app listening on port 3000!');
+})
--- server.js
+++ server.js
@@ -14,13 +14,13 @@ const session = require('express-session');
 const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
-    extended: true
+	extended: true
 }))
 
 app.use(session({
-    secret: 'secret',
-    resave: true,
-    saveUninitialized: true
+	secret: 'secret',
+	resave: true,
+	saveUninitialized: true
 }));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -31,18 +31,18 @@ MongoClient.connect(url, function (err, db) {
     var dbo = db.db('firstdb');
     // var query={username:"160120733050"};
     // dbo.collection("login").find(query).toArray(function(err,result){
-    //     if (err) throw err;
-    //     console.log(result);
-    //     console.log(result[0].username);
-    //     console.log(result[0].password);
-    //     db.close;
-    // });
+        //     if (err) throw err;
+        //     console.log(result);
+        //     console.log(result[0].username);
+        //     console.log(result[0].password);
+        //     db.close;
+        // });
     // to create a collection
     // dbo.createCollection("login",function(err,res){
-    //     if(err)throw err;
-    //     console.log('collection created');
-    //     db.close();
-    // });
+        //     if(err)throw err;
+        //     console.log('collection created');
+        //     db.close();
+        // });
     //to insert 
     // var myobj = {  tid: "1001",sid:"160120733003",hour:"1",date:"08-06-2022", present:"1"};
     // dbo.collection("attendance").insertOne(myobj,function(err,res){
@@ -50,7 +50,7 @@ MongoClient.connect(url, function (err, db) {
     //         console.log("1 document inserted");
     //         db.close;
     //     });
-
+        
     //to delete
     // var myquery={date:"10-06-2022"};
     // dbo.collection("student").deleteMany(myquery,function(err,obj){
@@ -58,6 +58,6 @@ MongoClient.connect(url, function (err, db) {
     //         console.log('values deleted');
     //         db.close();
     //     });
-});
+    });
 
 app.listen(5000);
